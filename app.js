02b1c14fdb68f6c1e68507d2d6b0000f290412af
Demian Ruban <rubandemian@gmail.com>
2022-06-09 19:46:23 +0200

    Improved js code formatting and id names
    JS code style was polished using "standard" npm package
    and "ale" vim plugin + some minor improvments on code
    readability.

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
-function sendImg() {
-    /*TODO:
-	- make error no link (toast message from Bootstrap)
-    */
-    let imgUrl = document.getElementById("basic-url").value;
+const button = document.getElementById('submit-button')
+button.onclick = sendImg
 
-    if (!imgUrl) { // if there is no link
-	console.log("No URL passed");
-    } else if (imgUrl.match('^(ftp|http|https):\/\/[^ "]+$')) {
-	console.log("URL matched");
-	const imageInfoDiv = document.getElementById("imageInfo");
-	const infoHeader = imageInfoDiv.children[0];
+function sendImg () {
+  /* TODO:
+  - make error no link (toast message from Bootstrap)
+  */
+  const imgUrl = document.getElementById('image-url').value
+  console.log('imgUrl: ' + Boolean(imgUrl))
 
-	let imgUrl = document.getElementById("basic-url").value;
-	console.log("Processed url: " + imgUrl);
+  if (!imgUrl) { // if there is no link
+    throw new Error('No URL passed')
+  } else if (imgUrl.match('^(ftp|http|https)://[^ "]+$')) {
 
-	const options = {
-		method: 'POST',
-		headers: {
-			'content-type': 'application/json',
-			'X-RapidAPI-Host': 'image-labeling1.p.rapidapi.com',
-			'X-RapidAPI-Key': '' // paste here api key secretly
-		},
-		body: `{"url":"${imgUrl}"}`
-	};
-
-	console.log("Sending imgUrl and getting response...");
-	fetch('https://image-labeling1.p.rapidapi.com/img/label', options) // getting response
-	    .then(response => response.json())
-	    .then(response => {
-		while (imageInfoDiv.childNodes.length > 5) {
-		    imageInfoDiv.removeChild(imageInfoDiv.lastChild);
-		}
-		console.log(response);
-		infoHeader.textContent = "Info";
-		for (const label in response) { //enumerating (iterating through indexes)
-		    let match = response[label];
-		    // take only two numbers after point into string
-		    let strmatch = match.toString().slice(2, 4);
-		    let labelPara = document.createElement('p')
-		    labelPara.textContent = label + ": " + strmatch + "%";
-		    imageInfoDiv.appendChild(labelPara);
-		}
-	    })
-	    .catch(err => console.error(err));
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Host': 'image-labeling1.p.rapidapi.com',
+        'X-RapidAPI-Key': '' // paste here api key secretly
+      },
+      body: `{'url':'${imgUrl}'}`
     }
+
+    console.log('Sending imgUrl and getting response...')
+    fetch('https://image-labeling1.p.rapidapi.com/img/label', options) // getting response
+      .then(response => response.json())
+      .then(processResponse)
+      .catch(err => console.error(err))
+  }
+}
+
+function processResponse (response) {
+  const imageInfoDiv = document.getElementById('image-info')
+  const infoHeader = imageInfoDiv.children[0]
+
+  console.log(response)
+
+  while (imageInfoDiv.childNodes.length > 5) { // counted nodes by myself NOT SCALABLE!!!
+    imageInfoDiv.removeChild(imageInfoDiv.lastChild)
+  }
+  infoHeader.textContent = 'Info'
+  for (const label in response) { // enumerating (iterating through indexes)
+    // take only two numbers after point into string
+    const strmatch = response[label].toString().slice(2, 4)
+    const labelPara = document.createElement('p')
+    labelPara.textContent = label + ': ' + strmatch + '%'
+    imageInfoDiv.appendChild(labelPara)
+  }
 }
